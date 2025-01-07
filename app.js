21602f321d1de25d3377048cd46410cfa25147fa
koloso-s <zarychkrystian@gmail.com>
2025-01-07 09:42:07 +0100

    create result function

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 const computerChoiceDisplay = document.getElementById('computer-choice');
 const userChoiceDisplay = document.getElementById('user-choice');
-const result = document.getElementById('result');
+const resultDisplay = document.getElementById('result');
 const buttons = document.querySelectorAll('button');
 let userChoice;
 let computerChoice;
+let result;
 
 buttons.forEach(choice=> choice.addEventListener("click",(e)=>{
     userChoice = e.target.id;
     userChoiceDisplay.innerHTML = userChoice;
     generateComputerChoice();
+    generateResult();
 }))
 
 function generateComputerChoice(){
@@ -30,4 +32,28 @@ function generateComputerChoice(){
             break;
     }
     computerChoiceDisplay.innerHTML = computerChoice;
+}
+
+function generateResult(){
+    if(userChoice == computerChoice)result = "Draw";
+    switch (userChoice) {
+        case "rock":
+            if(computerChoice == "paper")result = "computer win";
+            if(computerChoice == "scissors")result= "player win";
+            break;
+
+        case "paper":
+            if(computerChoice == "scissors")result = "computer win";
+            if(computerChoice == "rock")result = "player win";
+            break;  
+
+        case "scissors":
+            if(computerChoice == "rock")result = "computer win";
+            if(computerChoice == "paper")result = "player win";
+            break;
+
+        default:
+            break;
+    }
+    resultDisplay.innerHTML = result;
 }
