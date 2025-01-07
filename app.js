5c206ade78655d3c9f48c00c1c7aad255e43dcc4
koloso-s <zarychkrystian@gmail.com>
2025-01-07 09:30:15 +0100

    create computer select function

--- app.js
+++ app.js
@@ -3,9 +3,31 @@ const userChoiceDisplay = document.getElementById('user-choice');
 const result = document.getElementById('result');
 const buttons = document.querySelectorAll('button');
 let userChoice;
+let computerChoice;
 
 buttons.forEach(choice=> choice.addEventListener("click",(e)=>{
     userChoice = e.target.id;
     userChoiceDisplay.innerHTML = userChoice;
+    generateComputerChoice();
 }))
 
+function generateComputerChoice(){
+    const randomNumber = Math.floor(Math.random()*3)
+    switch (randomNumber) {
+        case 0:
+            computerChoice = "rock";
+            break;
+
+        case 1:
+            computerChoice = "paper";
+            break;
+
+        case 2:
+            computerChoice = "scissors";
+            break;
+
+        default:
+            break;
+    }
+    computerChoiceDisplay.innerHTML = computerChoice;
+}
